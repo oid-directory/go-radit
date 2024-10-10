2e683fb2ade6a41636df0d0f0ed4ea13f2eb7838
Jesse Coretta <{ID}+{username}@users.noreply.github.com>
2024-10-09 22:03:39 -0700

    inaugural (dev) release

--- doc.go
+++ doc.go
@@ -9,6 +9,8 @@ well over 125000 entries, comprised of both *[radir.Registration] and
 
 See the [RADIT I-D] for details.
 
+Please note this is a very early release; breaking changes are likely!
+
 [radir]: https://github.com/oid-directory/go-radir
 [RADIT I-D]: https://datatracker.ietf.org/doc/html/draft-coretta-oiddir-radit
 */
