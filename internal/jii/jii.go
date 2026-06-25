ce26b86c940d02d3ca0434e0811b50a7f75ac688
JesseCoretta <74126726+JesseCoretta@users.noreply.github.com>
2026-06-24 19:44:51 -0700

    actions sucks

--- internal/jii/jii.go
+++
@@ -1,8 +0,0 @@
-package jii
-
-/*
-github actions fails to build any package that contains only
-variables (such as this package). Thus we'll just add an empty
-func to work around this lame situation.
-*/
-func satisfyGithubActions() {}
